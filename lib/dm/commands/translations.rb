7d28479d2e845b2974043ceed42bc917d4f380e7
Jeroen van Dijk <jeroen@jeevidee.nl>
2009-02-21 10:38:16 +0100

    Reformatted translations and removed debug statement

--- lib/dm/commands/translations.rb
+++ lib/dm/commands/translations.rb
@@ -1,14 +1,11 @@
 module DM #:nodoc:
   module Generator #:nodoc:
     module Commands #:nodoc:
+      
       module Create
-				def translate(models, relative_path, locale = "en")
-          edit_translations(relative_path, locale) { |current_translations| 
-            test = build_translations(models).deep_merge(current_translations) 
-            puts test.to_yaml
-            test
-            }
-				end
+        def translate(models, relative_path, locale = "en")
+          edit_translations(relative_path, locale) { |current_translations| build_translations(models).deep_merge(current_translations) }
+        end
       end
 
       module Destroy
@@ -22,22 +19,22 @@ module DM #:nodoc:
             current_translations
           end
         end
-			end
-			
+      end
+      
       module List
         def translate(models, relative_path, locale = "en")
-					logger.update (relative_path)
+          logger.update (relative_path)
         end
       end
       
       module Utilities
         def edit_translations(relative_path, locale)
-  			    path = destination_path(relative_path)
+            path = destination_path(relative_path)
 
-  			    translations =  File.exists?(path) ? YAML::load_file(path) : 
-  			                                         {locale => {"activerecord" => { "models" => {}, "attributes" =>  {} } } }
-			    
-  			    translations[locale]["activerecord"] = yield(translations[locale]["activerecord"].clone)
+            translations =  File.exists?(path) ? YAML::load_file(path) : 
+                                                 {locale => {"activerecord" => { "models" => {}, "attributes" =>  {} } } }
+
+            translations[locale]["activerecord"] = yield(translations[locale]["activerecord"].clone)
 
             if translations[locale]["activerecord"]["models"].any?
               logger.update(relative_path) do
@@ -55,13 +52,13 @@ module DM #:nodoc:
         def build_translations(models)
           model_translations = {}
           attribute_translations = {}
-        
+
           models.each do |model|
             model_translations[ model.singular_name ] = model.singular_name
             attribute_translations[model.singular_name] = {}
             model.attributes.each { |attribute| attribute_translations[model.singular_name][attribute.name] = attribute.name }
           end
-        
+
           { "models" => model_translations, "attributes" =>  attribute_translations }
         end
       end
@@ -70,7 +67,7 @@ module DM #:nodoc:
 end
 
 %w(Create Destroy List).each do |action|
-	eval("DM::Generator::Commands::#{action}").send :include, DM::Generator::Commands::Utilities
+  eval("DM::Generator::Commands::#{action}").send :include, DM::Generator::Commands::Utilities
 end
 
 Rails::Generator::Commands::Create.send   :include,  DM::Generator::Commands::Create
